972d382c9981fd6eb52515f1c0838f4dc0ba8472
cyw9b9k8v9-design <cyw9b9k8v9@privaterelay.appleid.com>
2026-07-08 17:43:19 +0800

    彻底禁用mp4文件处理

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    assetsInlineLimit: 0,
     rollupOptions: {
       external: [
         /\.mp4$/
